e6fae2e5421686e9ed08b13682159f40d5436108
Dylan Tapp <jdylantapp@gmail.com>
2026-04-04 23:08:42 -0400

    added cors configuration for allowed connection to frontend

--- meal-recipes-backend/src/main/java/com/jdylantapp/meal_recipes_backend/config/SecurityConfig.java
+++ meal-recipes-backend/src/main/java/com/jdylantapp/meal_recipes_backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jdylantapp.meal_recipes_backend.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jdylantapp.meal_recipes_backend.filter.JwtAuthFilter;
 
@@ -38,7 +43,7 @@ public class SecurityConfig {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
-        http.cors(cors -> cors.disable());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
     }
@@ -52,6 +57,18 @@ public class SecurityConfig {
     @Bean
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     
